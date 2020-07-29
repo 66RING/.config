bca5c59519da6435812f7bd44ab30651614fa69d
66RING <1552087953@qq.com>
2020-07-29 22:22:26 +0800

    lfcd and mpv keybind

--- lf/lfcd.sh
+++ lf/lfcd.sh
@@ -16,10 +16,12 @@
 
 lfcd () {
     tmp="$(mktemp)"
+    export LF_BACK="$(pwd)"
     lf -last-dir-path="$tmp" "$@"
     if [ -f "$tmp" ]; then
         dir="$(cat "$tmp")"
         rm -f "$tmp"
+        unset LF_BACK
         if [ -d "$dir" ]; then
             if [ "$dir" != "$(pwd)" ]; then
                 cd "$dir"
