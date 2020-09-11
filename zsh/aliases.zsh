72dfbe86951ed8093914c670a177e6a41f50e501
66RING <1552087953@qq.com>
2020-09-11 23:25:48 +0800

    some useful update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -19,21 +19,21 @@ alias m='neomutt'
 alias md='mkdir'
 alias mf='touch'
 alias pc='proxychains'
-alias qrcp='qrcp -k'
+# alias qrcp='qrcp -k'
 alias ra='ranger'
 alias sctl='systemctl'
 alias s='neofetch | lolcat'
 alias top='gotop'
-alias vim='/usr/bin/nvim'
-alias vi='/usr/bin/nvim'
+alias vim='nvim'
+alias vi='nvim'
 alias yg='you-get'
 
 
 #************************
 #* tools
 #************************
-alias venvon='source /usr/bin/virtualenvwrapper.sh'
+alias venvon="source $(which virtualenvwrapper.sh)"
 alias proxyon='export http_proxy="127.0.0.1:12333" export https_proxy="127.0.0.1:12333"'
 alias proxyoff='unset http_proxy ; unset export https_proxy'
 
-alias tsr='/home/ring/scripts/tools/ai_trans_demo.py'
+alias tsr="$HOME/scripts/tools/ai_trans_demo.py"
