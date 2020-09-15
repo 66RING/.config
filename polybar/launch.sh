083b8cda6e226f4732b669f860488059efa29d89
66RING <1552087953@qq.com>
2020-09-15 20:33:00 +0800

    bspwm about multi monitor

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
 
-# Launch bar1 and bar2
+# IsPrimary=1
+MONITORs=$(polybar -m|sed -e 's/:.*$//g')
 
-export MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g')
+cat /tmp/polybar1.log > /tmp/polybar2.log
+printf "===== Polybar log =====\n\n" > /tmp/polybar1.log &
 
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar primary >>/tmp/polybar1.log 2>&1 &
+for m in $MONITORs; do
+    bspc monitor $m -d 1 2 3 4 5 6 7 8 9 10
 
-echo "Bars launched..."
+    export MONITOR=$m
+    # if [ "$IsPrimary" -eq 1 ]; then
+        polybar primary >> /tmp/polybar1.log 2>&1 &
+        # ((IsPrimary++))
+    # else
+        # polybar secondary >> /tmp/polybar1.log 2>&1 &
+    # fi
+    unset MONITOR
+done
+
