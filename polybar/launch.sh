20e200760200b2b01dbb56808166d198b532ad03
66RING <1552087953@qq.com>
2020-09-18 20:14:37 +0800

    polybar script and picom config update

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,23 +1,19 @@
 #!/usr/bin/env bash
 
-killall -q polybar
-
-# IsPrimary=1
-MONITORs=$(polybar -m|sed -e 's/:.*$//g')
+while [ ! -z "$(pgrep polybar)" ]; do
+    killall polybar
+done
 
 cat /tmp/polybar1.log > /tmp/polybar2.log
 printf "===== Polybar log =====\n\n" > /tmp/polybar1.log &
 
-for m in $MONITORs; do
+if type "xrandr"; then
+  # for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+  for m in $(polybar -m|sed -e 's/:.*$//g'); do  # with a priority
     bspc monitor $m -d 1 2 3 4 5 6 7 8 9 10
-
-    export MONITOR=$m
-    # if [ "$IsPrimary" -eq 1 ]; then
-        polybar primary >> /tmp/polybar1.log 2>&1 &
-        # ((IsPrimary++))
-    # else
-        # polybar secondary >> /tmp/polybar1.log 2>&1 &
-    # fi
-    unset MONITOR
-done
-
+    MONITOR=$m polybar --reload primary >> /tmp/polybar1.log 2>&1 &
+  done
+else
+  bspc monitor -d 1 2 3 4 5 6 7 8 9 10
+  polybar --reload primary >> /tmp/polybar1.log 2>&1 &
+fi
